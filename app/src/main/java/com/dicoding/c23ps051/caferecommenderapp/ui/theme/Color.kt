63ad9ea3131f30d7ef57a924d0eb066f8f4f5c80
Yen <a032dsy3371@bangkit.academy>
2023-05-23 00:51:12 +0700

    Adjust resources: colors and strings

--- app/src/main/java/com/dicoding/c23ps051/caferecommenderapp/ui/theme/Color.kt
+++ app/src/main/java/com/dicoding/c23ps051/caferecommenderapp/ui/theme/Color.kt
@@ -1,8 +1,13 @@
 package com.dicoding.c23ps051.caferecommenderapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import com.dicoding.c23ps051.caferecommenderapp.R
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Black = Color(R.color.black)
+val White = Color(R.color.white)
+val LightBlack = Color(R.color.light_black)
+val Brown = Color(R.color.brown)
+val Cream = Color(R.color.cream)
+val LightGray = Color(R.color.light_gray)
+val MildWhite = Color(R.color.mild_white)
+val DarkBrown = Color(R.color.dark_brown)
