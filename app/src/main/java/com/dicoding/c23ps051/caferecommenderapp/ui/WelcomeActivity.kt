b1352b34a6079e4d5b65e654a457edd0ae9a8c8a
Yen <a032dsy3371@bangkit.academy>
2023-05-25 01:44:42 +0700

    add welcome page

---
+++ app/src/main/java/com/dicoding/c23ps051/caferecommenderapp/ui/WelcomeActivity.kt
@@ -0,0 +1,106 @@
+package com.dicoding.c23ps051.caferecommenderapp.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.dicoding.c23ps051.caferecommenderapp.R
+import com.dicoding.c23ps051.caferecommenderapp.ui.components.AppLogo
+import com.dicoding.c23ps051.caferecommenderapp.ui.components.Button
+import com.dicoding.c23ps051.caferecommenderapp.ui.theme.Black
+import com.dicoding.c23ps051.caferecommenderapp.ui.theme.CafeRecommenderAppTheme
+import com.dicoding.c23ps051.caferecommenderapp.ui.theme.Cream
+
+class WelcomeActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CafeRecommenderAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background,
+                ) {
+                    Welcome()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Welcome(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.image),
+            contentDescription = stringResource(id = R.string.app_logo),
+            modifier = modifier
+                .size(256.dp)
+                .align(Alignment.CenterHorizontally),
+        )
+        Text(
+            text = stringResource(id = R.string.welcome_text),
+            style = MaterialTheme.typography.h5,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        Text(
+            text = stringResource(id = R.string.welcome_description),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.welcome_closing),
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.W700,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(64.dp))
+        Button(text = stringResource(id = R.string.sign_in)) {
+            /* TODO */
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Button(
+            text = stringResource(id = R.string.sign_up),
+            color = Cream,
+            textColor = Black,
+        ) {
+            /* TODO */
+        }
+    }
+}
