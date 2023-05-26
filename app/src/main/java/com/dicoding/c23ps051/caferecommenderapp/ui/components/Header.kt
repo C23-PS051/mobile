81adc192d75b847478424bba4ea1c061c763a42d
Yen <a032dsy3371@bangkit.academy>
2023-05-27 00:50:28 +0700

    fix header background and padding, adjust theme colors

--- app/src/main/java/com/dicoding/c23ps051/caferecommenderapp/ui/components/Header.kt
+++ app/src/main/java/com/dicoding/c23ps051/caferecommenderapp/ui/components/Header.kt
@@ -1,11 +1,14 @@
 package com.dicoding.c23ps051.caferecommenderapp.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,7 +29,9 @@ fun Header(
 ) {
     Row (
         verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier.padding(16.dp)
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.background)
+            .padding(horizontal = 16.dp, vertical = 12.dp)
     ){
         Image(
             painter = painterResource(id = R.drawable.location),
