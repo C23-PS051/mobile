9eff6de4b0d23ff866f7985acfab91b498729f8b
Yen <a032dsy3371@bangkit.academy>
2023-06-08 20:00:51 +0700

    remove unused constants

--- app/src/main/java/com/dicoding/c23ps051/caferecommenderapp/constants/Constants.kt
+++ app/src/main/java/com/dicoding/c23ps051/caferecommenderapp/constants/Constants.kt
@@ -1,7 +1,3 @@
 package com.dicoding.c23ps051.caferecommenderapp.constants
 
 const val MIN_PASSWORD_LENGTH = 8
-const val UNKNOWN = "unknown"
-const val FAILED = "failed"
-const val NOT_GRANTED = "not_granted"
-const val KNOWN = "known"
